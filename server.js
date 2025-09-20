f960cae2a8642dce67dcff7aae700b25da2fd47b
Firdavs001100 <foruseofbusiness@gmail.com>
2025-09-20 18:50:03 +0900

    feat: add author page and finish main page

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ console.log("Web server is starting...");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs")
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if (err) {
+        console.log("ERROR: ", err);
+    } else {
+        user = JSON.parse(data);
+    }
+});
 
 //1: Kirish code
 app.use(express.static("public"));
@@ -16,7 +26,6 @@ app.set("view engine", "ejs");
 
 //4: Routing code
 app.post("/addItem", (req, res) => {
-    console.log(req.body);
     res.json({ test: "success"});
 });
 
@@ -26,6 +35,12 @@ app.get("/", (req, res) => {
 
 app.get("/gift", (req, res) => {
     res.send(`<h1>Gift Page</h1>`);
+});
+
+app.get("/author", (req, res) => {
+    res.render("author", {
+        user: user
+    });
 });
 
 const server = http.createServer(app);
